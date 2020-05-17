505e1f3ba46ae03fa395dad9869eabd35394c588
hutomosaleh <61884180+hutomosaleh@users.noreply.github.com>
2020-05-17 13:28:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import discord
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GIPHY_TOKEN = os.getenv('GIPHY_TOKEN')
+
+bot = commands.Bot(command_prefix=['oi ', 'Oi ', 'OI ', 'jid '])
+
+startup_extensions = ['cogs.simple', 'cogs.ragnarok']
+
+
+@bot.event
+async def on_ready():
+    print('==============')
+    print('Jidad is ready')
+    print('==============')
+
+
+if __name__ == '__main__':
+    for extension in startup_extensions:
+        bot.load_extension(extension)
+
+bot.run(TOKEN, bot=True, reconnect=True)
